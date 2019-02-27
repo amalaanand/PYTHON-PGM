9e224c43e8f80178ff6c17226f13ffb15ba32a5a
amalaanand <amalaanand2k17@gmail.com>
2019-02-27 12:33:31 +0530

    Web designing

--- app.py
+++ app.py
@@ -3,5 +3,15 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "Hello...."
+@app.route("/home")
+def home():
+    return "Welcome to my home"
+@app.route("/contact")
+def contact():
+    return "Contact Us:222222"
+@app.route("/about")
+def about():
+    return "SNIT Vadakkevila,Kollam"        
+
 if(__name__=="__main__"):
     app.run()
